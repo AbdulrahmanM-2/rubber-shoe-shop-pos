b2326132749b423ae435347989d8fdd96d83c3c5
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-02 17:12:32 +0300

    Create ProductSearch.jsx

---
+++ pos-ui/src/components/ProductSearch.jsx
@@ -0,0 +1,25 @@
+export default function ProductSearch({ variants, addToCart }) {
+  return (
+    <div>
+      <h3>Products</h3>
+      {variants.map((v) => (
+        <div key={v.id}>
+          {v.product.name} - {v.size}/{v.color} - {v.sellingPrice} Tsh
+          <button onClick={() => addToCart(v)}>Add</button>
+        </div>
+      ))}
+    </div>
+  );
+}export default function ProductSearch({ variants, addToCart }) {
+  return (
+    <div>
+      <h3>Products</h3>
+      {variants.map((v) => (
+        <div key={v.id}>
+          {v.product.name} - {v.size}/{v.color} - {v.sellingPrice} Tsh
+          <button onClick={() => addToCart(v)}>Add</button>
+        </div>
+      ))}
+    </div>
+  );
+}
