1188ca51f4f0fbcc9388c62296396a5dd793c47d
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-02 13:54:17 +0300

    Add ProductVariant entity with attributes

---
+++ ProductVariant.java
@@ -0,0 +1,17 @@
+@Entity
+public class ProductVariant {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @ManyToOne
+  private Product product;
+
+  private String size;
+  private String color;
+  private BigDecimal costPrice;
+  private BigDecimal sellingPrice;
+  private Integer quantity;
+  private Integer reorderLevel;
+}
