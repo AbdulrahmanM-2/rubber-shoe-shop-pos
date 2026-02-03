d810afbd86438920d160349972f71a0ff7097552
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-03 06:12:58 +0300

    Update ProductVariant.java

--- product/ProductVariant.java
+++ product/ProductVariant.java
@@ -1,1 +1,18 @@
 
+@Entity
+public class ProductVariant {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @ManyToOne
+  private Product product;
+
+  private String size;
+  private String color;
+  private BigDecimal costPrice;
+  private BigDecimal sellingPrice;
+  private Integer quantity;
+  private Integer reorderLevel;
+}
