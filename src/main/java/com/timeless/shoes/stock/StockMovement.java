75afbfcf59260b6e92472acbdab6612bbc89355e
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-04 00:29:17 +0300

    Update StockMovement.java

--- src/main/java/com/timeless/shoes/stock/StockMovement.java
+++ src/main/java/com/timeless/shoes/stock/StockMovement.java
@@ -1,1 +1,16 @@
+@Entity
+public class StockMovement {
 
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @ManyToOne
+  private ProductVariant variant;
+
+  private String type; // IN, OUT
+  private Integer quantity;
+  private String reference;
+
+  private LocalDateTime createdAt = LocalDateTime.now();
+}
