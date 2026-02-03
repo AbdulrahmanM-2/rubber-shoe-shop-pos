93cda41bf1845c153467d26cc4078da4c16ffca8
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-03 05:54:38 +0300

    Delete main.java

--- main.java
+++
@@ -1,6 +0,0 @@
-@SpringBootApplication
-public class RubberShoesShopApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(RubberShoesShopApplication.class, args);
-  }
-}
