0fdcb94f6f61b4b9b2046a0aa553a4c82c05040c
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-03 18:29:47 +0300

    Update OrderRepository.java

--- repository/OrderRepository.java
+++ repository/OrderRepository.java
@@ -1,1 +1,15 @@
+package com.timeless.shoes.repository;
 
+import com.timeless.shoes.model.Order;
+import com.timeless.shoes.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByCashier(User cashier);
+
+    List<Order> findByOrderTimeBetween(LocalDateTime start, LocalDateTime end);
+}
