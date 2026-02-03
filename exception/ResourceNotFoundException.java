70e578071f7fe417adaf9b2b674819e3b6fe43ee
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-03 18:42:31 +0300

    Update ResourceNotFoundException.java

--- exception/ResourceNotFoundException.java
+++ exception/ResourceNotFoundException.java
@@ -1,1 +1,7 @@
+package com.timeless.shoes.exception;
 
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
