cb0e796c8260c5703c9d49103f95a7781a76ba30
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-03 20:01:00 +0300

    Create PinAuthenticationProvider.java

---
+++ config/Security/PinAuthenticationProvider.java
@@ -0,0 +1,42 @@
+package com.timeless.shoes.security;
+
+import com.timeless.shoes.model.User;
+import com.timeless.shoes.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class PinAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserService userService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+
+        String phone = authentication.getPrincipal().toString();
+        String pin = authentication.getCredentials().toString();
+
+        User user = userService.getActiveUserByPhone(phone);
+
+        if (!userService.verifyPin(user, pin)) {
+            throw new BadCredentialsException("Invalid PIN");
+        }
+
+        return new PinAuthenticationToken(
+                user,
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return PinAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
