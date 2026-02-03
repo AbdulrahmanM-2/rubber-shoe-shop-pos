e399414224d0ae5971e8b6a688764ca99c97b789
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-03 22:20:42 +0300

    Update User.java

--- src/main/java/com/timeless/shoes/user/User.java
+++ src/main/java/com/timeless/shoes/user/User.java
@@ -1,1 +1,15 @@
+@Entity
+@Table(name = "users")
+@Getter @Setter
+public class User {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String fullName;
+  private String username;
+  private String passwordHash;
+  private String role; // OWNER, MANAGER, CASHIER
+  private boolean active = true;
+}
