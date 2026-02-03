225e355ffb603f8b94573d77a7aed4c55d6aa51b
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-03 06:00:24 +0300

    Update Order.java

--- model/Order.java
+++ model/Order.java
@@ -1,1 +1,34 @@
 
+package com.timeless.shoes.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime orderTime;
+
+    @ManyToOne
+    @JoinColumn(name = "cashier_id")
+    private User cashier;
+
+    private BigDecimal totalAmount;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items;
+}
