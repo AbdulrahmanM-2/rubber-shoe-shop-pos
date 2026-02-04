f8bc9d16c00786e8a2a3b61b20bc6a5d394088fa
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-04 14:04:36 +0300

    Create ProfitReportDto.java

---
+++ src/main/java/com/timeless/shoes/dto/ProfitReportDto.java
@@ -0,0 +1,12 @@
+package com.timeless.shoes.dto;
+
+public class LowStockReportDto {
+    private Long variantId;
+    private String productName;
+    private String size;
+    private String color;
+    private int quantity;
+    private int reorderLevel;
+
+    // Getters & setters
+}
