056317de2f703321daca853bca064542652d718c
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-04 14:03:55 +0300

    Create CustomerDto.java

---
+++ src/main/java/com/timeless/shoes/dto/CustomerDto.java
@@ -0,0 +1,12 @@
+package com.timeless.shoes.dto;
+
+import java.math.BigDecimal;
+
+public class CustomerDto {
+    private Long id;
+    private String name;
+    private String phone;
+    private BigDecimal creditLimit;
+
+    // Getters & setters
+}
