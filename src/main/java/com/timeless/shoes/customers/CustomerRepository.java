52987137f29d091619737576b49d9015633d111e
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-05 21:59:41 +0300

    Update CustomerRepository.java

--- src/main/java/com/timeless/shoes/customers/CustomerRepository.java
+++ src/main/java/com/timeless/shoes/customers/CustomerRepository.java
@@ -1,14 +1,6 @@
-package com.timeless.shoes.customers;
+package com.timeless.shoes.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-    Optional<Customer> findByPhone(String phone);
-
-    boolean existsByPhone(String phone);
 }
