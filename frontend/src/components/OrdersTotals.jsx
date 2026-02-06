9b9a293cb81b2d0ae94201675ebbb5b7d964fa35
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-06 14:27:48 +0300

    Update OrdersTotals.jsx

--- frontend/src/components/OrdersTotals.jsx
+++ frontend/src/components/OrdersTotals.jsx
@@ -1,11 +1,11 @@
 const OrderTotals = ({ order, messages }) => (
-  <div className="order-totals">
-    <h3>{messages.subtotal} / {messages.totalPaid} / {messages.balance}</h3>
-    <p>{messages.subtotal}: {order.subtotal}</p>
-    <p>{messages.totalPaid}: {order.totalPaid}</p>
-    <p>{messages.balance}: {order.balance}</p>
-    <p>{messages.orderStatus}: {order.status}</p>
-  </div>
+  <div className="space-y-2">
+  <h3 className="text-lg font-semibold">{messages.subtotal} / {messages.totalPaid} / {messages.balance}</h3>
+  <p>{messages.subtotal}: <span className="font-bold">{order.subtotal}</span></p>
+  <p>{messages.totalPaid}: <span className="font-bold">{order.totalPaid}</span></p>
+  <p>{messages.balance}: <span className="font-bold">{order.balance}</span></p>
+  <p>{messages.orderStatus}: <span className="font-bold">{order.status}</span></p>
+</div>
 );
 
 export default OrderTotals;
