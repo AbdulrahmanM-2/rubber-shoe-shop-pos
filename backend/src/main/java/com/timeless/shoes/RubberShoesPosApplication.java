da8bf067eae7428a3448fb0b3863d55d9412b4e5
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-08 11:39:50 +0300

    Update RubberShoesPosApplication.java

--- backend/src/main/java/com/timeless/shoes/RubberShoesPosApplication.java
+++ backend/src/main/java/com/timeless/shoes/RubberShoesPosApplication.java
@@ -1,1 +1,12 @@
+package com.timeless.shoes;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubberShoesPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RubberShoesPosApplication.class, args);
+    }
+}
