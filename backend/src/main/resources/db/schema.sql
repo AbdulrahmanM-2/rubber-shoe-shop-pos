b5ce2dfcddab1bdf99ee4c0118802fd75040f469
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-08 13:34:50 +0300

    Add files via upload

---
+++ backend/src/main/resources/db/schema.sql
@@ -0,0 +1,13 @@
+-- Core
+\i schema/users.sql
+\i schema/products.sql
+  \i schema/productVariant.sql
+
+-- Transactions
+\i schema/orders.sql
+\i schema/payments.sql
+\i schema/suppliers.sql
+\i schema/sales.sql
+\i schema/StockMovements.sql
+\i schema/saleItem.sql
+\i schema/customers.sql
