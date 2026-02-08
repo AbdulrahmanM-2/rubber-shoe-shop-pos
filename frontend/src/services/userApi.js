eeee8e0d32e0e4730f73512995085cb85451705b
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-08 11:00:11 +0300

    Update userApi.js

--- frontend/src/services/userApi.js
+++ frontend/src/services/userApi.js
@@ -1,1 +1,2 @@
-
+const BASE = "http://localhost:8080/api/users";
+export const getUsers = () => fetch(BASE, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }}).then(r=>r.json());
