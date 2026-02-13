f264218fc0f6c16624e76db33b7dc4bf3516624c
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-13 23:19:16 +0300

    Delete backend/src/main/java/com/timeless/shoes/TimelessShoesApplication.java

--- backend/src/main/java/com/timeless/shoes/TimelessShoesApplication.java
+++
@@ -1,7 +0,0 @@
-@SpringBootApplication
-public class RubberShoesShopApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(RubberShoesShopApplication.class, args);
-  }
-}
-
