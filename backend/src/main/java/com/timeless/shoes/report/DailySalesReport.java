c99855b202a4e6924962dcf154571293dd5bea46
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-14 19:04:30 +0300

    Update DailySalesReport.java

--- backend/src/main/java/com/timeless/shoes/report/DailySalesReport.java
+++ backend/src/main/java/com/timeless/shoes/report/DailySalesReport.java
@@ -7,15 +7,16 @@ public class DailySalesReport {
 
     private LocalDate date;
     private BigDecimal totalSales;
-    private BigDecimal totalProfit;
+    private BigDecimal profit;
 
     public DailySalesReport() {}
 
     public LocalDate getDate() { return date; }
-    public BigDecimal getTotalSales() { return totalSales; }
-    public BigDecimal getTotalProfit() { return totalProfit; }
-
     public void setDate(LocalDate date) { this.date = date; }
+
+    public BigDecimal getTotalSales() { return totalSales; }
     public void setTotalSales(BigDecimal totalSales) { this.totalSales = totalSales; }
-    public void setTotalProfit(BigDecimal totalProfit) { this.totalProfit = totalProfit; }
+
+    public BigDecimal getProfit() { return profit; }
+    public void setProfit(BigDecimal profit) { this.profit = profit; }
 }
