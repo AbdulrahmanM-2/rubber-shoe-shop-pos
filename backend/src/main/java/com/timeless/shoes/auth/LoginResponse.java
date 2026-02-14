072a1b795ff1c0e5e68d815af9fe7c1c3809bb76
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-14 19:22:27 +0300

    Update LoginResponse.java

--- backend/src/main/java/com/timeless/shoes/auth/LoginResponse.java
+++ backend/src/main/java/com/timeless/shoes/auth/LoginResponse.java
@@ -1,15 +1,18 @@
 package com.timeless.shoes.auth;
 
 public class LoginResponse {
-    private String token;
+
     private String username;
     private String role;
 
-    public LoginResponse(String token, String username, String role) {
-        this.token = token;
+    public LoginResponse(String username, String role) {
         this.username = username;
         this.role = role;
     }
 
-    // Getters & setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
