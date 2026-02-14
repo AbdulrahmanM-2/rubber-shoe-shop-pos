3c3c1420e50802fbd9d6395e3ca2f89d55fe15d9
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-14 18:46:37 +0300

    Update User.java

--- backend/src/main/java/com/timeless/shoes/users/User.java
+++ backend/src/main/java/com/timeless/shoes/users/User.java
@@ -1,16 +1,6 @@
 package com.timeless.shoes.users;
 
 public class User {
-    private String username;
-    private String password;
-    private String role;
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public String getUsername() { return "user"; }
+    public String getRole() { return "ROLE_USER"; }
 }
