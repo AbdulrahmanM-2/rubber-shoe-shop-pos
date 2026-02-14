01ed94cfcab897f06359a0aae30c0c698d8f19ca
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-14 17:06:42 +0300

    Create ReportService.java

---
+++ backend/src/main/java/com/timeless/shoes/report/ReportService.java
@@ -0,0 +1,12 @@
+package com.timeless.shoes.report;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ReportService {
+
+    public List<DailySalesReport> getDailySales() {
+        return List.of();
+    }
+}
