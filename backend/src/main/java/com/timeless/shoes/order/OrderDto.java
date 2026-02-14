82e4dc0ddf5964ad5e5b93d8205258336c7eaa04
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-14 18:34:08 +0300

    Create OrderDto.java

---
+++ backend/src/main/java/com/timeless/shoes/order/OrderDto.java
@@ -0,0 +1,9 @@
+package com.timeless.shoes.order.dto;
+
+public class OrderDto {
+    private Long id;
+    private String orderNo;
+
+    public Long getId() { return id; }
+    public String getOrderNo() { return orderNo; }
+}
