a6d0cea82ee244e702b106be4193703c74914783
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-14 04:17:10 +0300

    Update CustomerRepository.java

--- backend/src/main/java/com/timeless/shoes/customers/CustomerRepository.java
+++ backend/src/main/java/com/timeless/shoes/customers/CustomerRepository.java
@@ -1,6 +1,8 @@
-package com.timeless.shoes.customer;
+package com.timeless.shoes.customers;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository {
+    Optional<Customer> findById(Long id);
+    long count();
 }
