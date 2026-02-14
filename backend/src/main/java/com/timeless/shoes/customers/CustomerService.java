d3a82bcba2f2066d0702c513cffdf29e17ec84a1
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-02-14 18:57:36 +0300

    Update CustomerService.java

--- backend/src/main/java/com/timeless/shoes/customers/CustomerService.java
+++ backend/src/main/java/com/timeless/shoes/customers/CustomerService.java
@@ -1,12 +1,14 @@
 package com.timeless.shoes.customers;
 
-import java.util.Collections;
-import java.util.List;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
-    public List<Object> getAllCustomers() {
-        return Collections.emptyList(); // stub
+
+    public List<Customer> getAllCustomers() {
+        // stub returns empty list
+        return List.of();
     }
 }
