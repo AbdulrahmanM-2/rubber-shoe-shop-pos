0dc90619e44a1acd19ec9e63f3a86ced8820d5cd
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-07 02:39:11 +0300

    Delete frontend directory

--- frontend/src/services/userApi.js
+++
@@ -1,2 +0,0 @@
-const BASE = "http://localhost:8080/api/users";
-export const getUsers = () => fetch(BASE, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }}).then(r=>r.json());
