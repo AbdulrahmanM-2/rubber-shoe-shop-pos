08203d4bfcd71ec25f10152fe61d4d4d03876421
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-08 02:24:57 +0300

    Add files via upload

--- rubber_shoe_pos/backend/src/main/java/com/timeless/shoes/repository/UserRepository.java
+++ rubber_shoe_pos/backend/src/main/java/com/timeless/shoes/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.timeless.shoes.repository;
-
-import com.timeless.shoes.users.User;
-import java.util.List;
-
-public interface UserRepository {
-    User findByUsername(String username);
-    List<User> findAll();
+import com.timeless.shoes.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
