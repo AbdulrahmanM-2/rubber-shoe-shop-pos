caa0f0f44bcf664d40430497a61b2eb4c53c9174
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-07 03:05:29 +0300

    Add files via upload

---
+++ rubber_shoe_pos/backend/src/main/java/com/timeless/shoes/config/security/PinAuthenticationProvider.java
@@ -0,0 +1,21 @@
+package com.timeless.shoes.security;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import com.timeless.shoes.users.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PinAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null; // stub
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+}
