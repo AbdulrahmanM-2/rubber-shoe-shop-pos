581023d8a23e1a3f4c8986f3387eaf5663cfcca9
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-08 04:04:26 +0300

    Add files via upload

--- rubber_shoe_pos/backend/src/main/java/com/timeless/shoes/customers/CustomerRepository.java
+++ rubber_shoe_pos/backend/src/main/java/com/timeless/shoes/customers/CustomerRepository.java
@@ -1,8 +1,10 @@
 package com.timeless.shoes.customers;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-public interface CustomerRepository {
-    Optional<Customer> findById(Long id);
-    long count();
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByPhoneNumber(String phoneNumber);
 }
