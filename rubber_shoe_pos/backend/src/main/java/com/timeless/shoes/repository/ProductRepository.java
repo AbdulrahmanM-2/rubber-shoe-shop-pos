4d6f2a69a6a3f00bb0dd3102734b4537736a6d90
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-08 04:08:23 +0300

    Add files via upload

--- rubber_shoe_pos/backend/src/main/java/com/timeless/shoes/repository/ProductRepository.java
+++ rubber_shoe_pos/backend/src/main/java/com/timeless/shoes/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.timeless.shoes.repository;
 
 import com.timeless.shoes.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
-public interface ProductRepository {
-    Product findById(Long id);
-    List<Product> findAll();
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByActiveTrue();
 }
