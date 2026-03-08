b92e88fc4bb7fd91b6871c63b3c108f3c0b223b7
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-03-08 04:16:48 +0300

    Add files via upload

---
+++ rubber_shoe_pos/backend/src/main/java/com/timeless/shoes/dto/OpenShiftRequest.java
@@ -0,0 +1,7 @@
+package com.timeless.shoes.dto;
+
+public class OpenShiftRequest {
+    private Double openingBalance;
+    public Double getOpeningBalance() { return openingBalance; }
+    public void setOpeningBalance(Double openingBalance) { this.openingBalance = openingBalance; }
+}
